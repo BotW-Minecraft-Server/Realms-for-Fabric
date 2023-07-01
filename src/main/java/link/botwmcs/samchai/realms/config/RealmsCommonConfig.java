068de15d7f7e69b1b954604162ddb0b8a7c59c23
Sam Chai <80421117@qq.com>
2023-07-01 18:45:50 +0900

    Update RealmsCommonConfig.java

--- src/main/java/link/botwmcs/samchai/realms/config/RealmsCommonConfig.java
+++ src/main/java/link/botwmcs/samchai/realms/config/RealmsCommonConfig.java
@@ -9,4 +9,5 @@ public class RealmsCommonConfig implements ConfigData {
     public boolean enable_single_player_function = false;
     public boolean enable_xp_economy = true;
     public boolean enable_ecohelper_hook = true;
+    public String announcement_url = "https://raw.githubusercontent.com/BotW-Minecraft-Server/Realms-for-Fabric/main/todays_announcement.txt";
 }
